d6963a8ef15ec12276234af01dd952b3859ced12
Vinícius Amaral <amaralvinicius171@gmail.com>
2021-10-21 15:49:55 -0300

    Application in loop

--- main.py
+++ main.py
@@ -14,35 +14,41 @@ import numpy as np
 from ReductionFunctions.calculus import coordReduction
 
 def main():
-    alpha = delta = date = np.array([])
+    run = True
 
-    while alpha.size == 0:
-        try:
-            alpha = np.array([float(input('Alpha hora: ')), float(input('Alpha minuto: ')), float(input('Alpha segundo: '))])
-        except ValueError:
-            print('Tipo numérico inválido\n')
+    while run:
+        alpha = delta = date = np.array([])
 
-    print(f'\nAlpha: {alpha[0]}:{alpha[1]}:{alpha[2]}\n')
+        while alpha.size == 0:
+            try:
+                alpha = np.array([float(input('Alpha hora: ')), float(input('Alpha minuto: ')), float(input('Alpha segundo: '))])
+            except ValueError:
+                print('Tipo numérico inválido\n')
 
-    while delta.size == 0:
-        try:
-            delta = np.array([float(input('Delta grau: ')), float(input('Delta minuto: ')), float(input('Delta segundo: '))])
-        except ValueError:
-            print('Tipo numérico inválido\n')
+        print(f'\nAlpha: {alpha[0]}:{alpha[1]}:{alpha[2]}\n')
 
-    print(f'\nDelta: {delta[0]}º {delta[1]}\' {delta[2]}\'\'\n')
+        while delta.size == 0:
+            try:
+                delta = np.array([float(input('Delta grau: ')), float(input('Delta minuto: ')), float(input('Delta segundo: '))])
+            except ValueError:
+                print('Tipo numérico inválido\n')
 
-    while date.size == 0:
-        try:
-            date  = np.array([int(input('Dia: ')), int(input('Mês: ')), int(input('Ano: '))])
-        except ValueError:
-            print('Tipo numérico inválido\n')
+        print(f'\nDelta: {delta[0]}º {delta[1]}\' {delta[2]}\'\'\n')
 
-    print(f'\nData: {date[0]}/{date[1]}/{date[2]}\n')
+        while date.size == 0:
+            try:
+                date  = np.array([int(input('Dia: ')), int(input('Mês: ')), int(input('Ano: '))])
+            except ValueError:
+                print('Tipo numérico inválido\n')
 
-    date  = np.array([2, 7, 2021])
+        print(f'\nData: {date[0]}/{date[1]}/{date[2]}\n')
 
-    coordReduction(alpha, delta, date)
-    
+        coordReduction(alpha, delta, date)
+
+        if input('\nDeseja usar a aplicação novamente? [S/N] ').upper() not in ['S', 'SIM', 'Y', 'YES']:
+            run = False
+        else:
+            print()
+
 if __name__ == '__main__':
     main()
