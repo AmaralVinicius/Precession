c887e71f4001e67fd0e95ba67ba5d6b3ca2cca60
Vinícius Amaral <amaralvinicius171@gmail.com>
2021-12-22 23:27:42 -0300

    Change in input system

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
     ############################ 
 
     - Alpha deve ser escrito no formato hh:mm:ss, por exemplo: 10:08:22.31099
-    - Delta deve ser escrito no formato gg:mm:ss, por exemplo: 11º 58' 01.9516''
+    - Delta deve ser escrito no formato gg:mm:ss, por exemplo: 11º 58' 01.9516"
     - Date deve ser escrito no formato DD:MM:AA, por exemplo: 2/7/2021
 '''
 
@@ -23,30 +23,63 @@ def main():
     elif platform.system() == 'Linux' or platform.system() == 'Darwin':
         os.system('clear')
 
-    while run:
-        alpha = delta = date = np.array([])
+    print('''- Alpha deve ser escrito no formato hh:mm:ss, por exemplo: 10:08:22.31099
+- Delta deve ser escrito no formato gg:mm:ss, por exemplo: 11º 58\' 01.9516"
+- Date deve ser escrito no formato DD:MM:AA, por exemplo: 2/7/2021\n''')
 
-        while alpha.size == 0:
-            try:
-                alpha = np.array([float(input('Alpha hora: ')), float(input('Alpha minuto: ')), float(input('Alpha segundo: '))])
-            except ValueError:
-                print('Tipo numérico inválido\n')
+    while run:
+        alpha =  list()
+        delta = list()
+        date = list()
+
+        while len(alpha) != 3:
+            alphaInput = str(input('Alpha: ')).replace(' ', '')
+
+            if alphaInput.count(':') == 2:
+                try:
+                    alpha.append(float(alphaInput[:alphaInput.find(':')]))
+                    alphaInput = alphaInput[alphaInput.find(':') + 1:]
+                    alpha.append(float(alphaInput[:alphaInput.find(':')]))
+                    alpha.append(float(alphaInput[alphaInput.find(':') + 1:]))
+                except ValueError:
+                    print('Tipos numéricos inválidos\n')
+                    alpha.clear()
+            else:
+                print('Formato inválido\n')
 
         print(f'\nAlpha: {alpha[0]}:{alpha[1]}:{alpha[2]}\n')
 
-        while delta.size == 0:
-            try:
-                delta = np.array([float(input('Delta grau: ')), float(input('Delta minuto: ')), float(input('Delta segundo: '))])
-            except ValueError:
-                print('Tipo numérico inválido\n')
+        while len(delta) != 3:
+            deltaInput = str(input('Delta: ')).replace(' ', '').replace(':', '').replace('º', ':').replace('\'', ':').replace('"', ':')
 
-        print(f'\nDelta: {delta[0]}º {delta[1]}\' {delta[2]}\'\'\n')
+            if deltaInput.count(':') == 3:
+                try:
+                    delta.append(float(deltaInput[:deltaInput.find(':')]))
+                    deltaInput = deltaInput[deltaInput.find(':') + 1:]
+                    delta.append(float(deltaInput[:deltaInput.find(':')]))
+                    delta.append(float(deltaInput[deltaInput.find(':') + 1:-1]))
+                except ValueError:
+                    print('Tipos numéricos inválidos\n')
+                    delta.clear()
+            else:
+                print('Formato inválido\n')
 
-        while date.size == 0:
-            try:
-                date  = np.array([int(input('Dia: ')), int(input('Mês: ')), int(input('Ano: '))])
-            except ValueError:
-                print('Tipo numérico inválido\n')
+        print(f'\nDelta: {delta[0]}º {delta[1]}\' {delta[2]}"\n')
+
+        while len(date) != 3:
+            dateInput = str(input('Date: ')).replace(' ', '')
+
+            if dateInput.count('/') == 2:
+                try:
+                    date.append(int(dateInput[:dateInput.find('/')]))
+                    dateInput = dateInput[dateInput.find('/') + 1:]
+                    date.append(int(dateInput[:dateInput.find('/')]))
+                    date.append(int(dateInput[dateInput.find('/') + 1:]))
+                except ValueError:
+                    print('Tipos numéricos inválidos\n')
+                    date.clear()
+            else:
+                print('Formato inválido\n')
 
         print(f'\nData: {date[0]}/{date[1]}/{date[2]}\n')
 
